6d61bff27289ba207147e15a25c761bef744fa83
Humanphyti <humanphyti@gmail.com>
2021-01-31 18:20:30 -0800

    Adding necessary Libraries

---
+++ OuroborosVandaleria/UserInterface/UIState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OuroborosVandaleria.UserInterface
+{
+    class UIState
+    {
+        public enum States
+        {
+            MAINMENU,
+
+        }
+    }
+}
